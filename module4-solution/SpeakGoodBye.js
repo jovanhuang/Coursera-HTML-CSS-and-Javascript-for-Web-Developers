afb4049d9f73be5b9d74dd2c78b944090da9fc86
jovanhuang <jhtc98@hotmail.com>
2020-06-01 23:14:58 +0800

    Completed Module 4

---
+++ module4-solution/SpeakGoodBye.js
@@ -0,0 +1,19 @@
+// NOTE! The steps in this file are basically identical to the ones you
+// performed in the SpeakHello.js file.
+
+// STEP 6: Wrap the entire contents of SpeakGoodBye.js inside of an IIFE
+// See Lecture 52, part 2
+
+(function (window) {
+	var byeSpeaker = {};
+	var speakWord = "Good Bye";
+	byeSpeaker.speak = function (name) {
+ 		console.log(speakWord + " " + name);
+	}
+
+// STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
+// 'byeSpeaker' on the global scope as well.
+// xxxx.xxxx = byeSpeaker;
+	window.byeSpeaker = byeSpeaker;
+}
+)(window);
